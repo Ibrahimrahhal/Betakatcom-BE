ee93e6086148ae427045d4e2d9fe1cd2190720ea
Ibrahim Rahhal <irahhal@atypon.com>
2021-07-08 02:41:02 +0300

    issue-5: Setup Github Basic Pull Automations

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,9 @@
-import error from './error';
+import error from "./error";
 
 export default class config {
-    private constructor() {}
-    public static get(configName: string): string {
-        if(typeof process.env[configName] != typeof undefined)
-            error.throw(`Requested Config Not Found:- ${configName}\n`);
-        return process.env[configName] as string;
-    }
+  private constructor() {}
+  public static get(configName: string): string {
+    if (typeof process.env[configName] != typeof undefined) error.throw(`Requested Config Not Found:- ${configName}\n`);
+    return process.env[configName] as string;
+  }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
-import _error from './error';
-import _config from './config';
+import _error from "./error";
+import _config from "./config";
 
 export const config = _config;
 export const error = _error;
-
