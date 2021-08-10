7aa61892112058bafe76e9b6deea4f37bd54c015
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2021-08-10 19:38:57 +0300

    issue-19: dockerize mysql database

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ image_name=betakatcom-be
 image_tag=dev
 container_name=betakatcom-be-dev
 dockerfile=dockerfile.dev
-
+listen_port=5000
 
 if [ -z "$(docker images -q $image_name:$image_tag)" ]; then
   docker build -t $image_name:$image_tag -f $dockerfile .
@@ -13,7 +13,7 @@ fi
 containerID=$(docker ps -aqf "name=$container_name")
 
 if [ -z "$containerID" ]; then
-    docker run --name $container_name -v "$(pwd)":/app -p 5000:5000 $image_name:$image_tag
+    docker run --name $container_name -v "$(pwd)":/app -p $listen_port:$listen_port $image_name:$image_tag
 else
     docker start -a $container_name
 fi
