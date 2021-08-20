85d35026a1e4be5e7a18c899366a2bbf1e215393
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2021-08-20 20:32:47 +0300

    issue-28: Creating the rest of the API's

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import user from "./user";
 import users from "./users";
 import authentication from "./authentication";
 import transaction from "./transaction";
-import cardType from './cardType';
+import cardType from "./cardType";
 
 export const PrivateRoutes = [user, users, transaction, cardType];
 export const PublicRoutes = [authentication];
