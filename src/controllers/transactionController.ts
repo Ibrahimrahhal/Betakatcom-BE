ecb4893d711ded4d7f7c6376ea92bc8062087438
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2021-12-16 17:55:04 +0200

    fixes

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -194,8 +194,8 @@ export default class TransactionController {
         {
           type: TranscationType.ballancePay,
           amount: -1 * amount,
-          createdBy: RecievingUser.get('id'),
-          userEffected: payingUser.get('id'),
+          createdBy: RecievingUser.get("id"),
+          userEffected: payingUser.get("id"),
         },
         {
           transaction: t,
@@ -205,8 +205,8 @@ export default class TransactionController {
         {
           type: TranscationType.payDept,
           amount: amount,
-          createdBy: RecievingUser.get('id'),
-          userEffected: payingUser.get('id'),
+          createdBy: RecievingUser.get("id"),
+          userEffected: payingUser.get("id"),
         },
         {
           transaction: t,
@@ -216,8 +216,8 @@ export default class TransactionController {
         {
           type: TranscationType.addDept,
           amount: amount,
-          createdBy: RecievingUser.get('id'),
-          userEffected: RecievingUser.get('id'),
+          createdBy: RecievingUser.get("id"),
+          userEffected: RecievingUser.get("id"),
         },
         {
           transaction: t,
